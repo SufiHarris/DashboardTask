8082aa767571d230fbf497b9a65704fe9f80d65a
SufiHarris <harrissofi02@gmail.com>
2024-04-17 14:03:59 +0530

    Initial Commit

---
+++ DashboardTask/DashboardTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashboardTaskApp.swift
+//  DashboardTask
+//
+//  Created by Mohammad Haris Sofi on 17/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DashboardTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
